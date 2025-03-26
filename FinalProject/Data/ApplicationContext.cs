12bd3b2ef754ec98e77c62f9b9d92f29aeabc1a6
Gres_gj98@student.itstep.org <Gres_gj98@student.itstep.org>
2025-03-26 13:43:27 +0200

    Added Smarter.ASP database and corrected database structure (deleted cascade removes)

--- FinalProject/Data/ApplicationContext.cs
+++ FinalProject/Data/ApplicationContext.cs
@@ -7,10 +7,33 @@ namespace FinalProject.Data
     {
         public ApplicationContext(DbContextOptions options) : base(options)
         {
+            Database.EnsureCreated();
             //Database.EnsureDeleted();
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Composition> Compositions { get; set; }
         public DbSet<Comment> Comments { get; set; }
-    }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Comment>()
+				.HasOne(c => c.User)
+				.WithMany(u => u.Comments)
+				.HasForeignKey(c => c.UserId)
+				.OnDelete(DeleteBehavior.Restrict); 
+
+			modelBuilder.Entity<Comment>()
+				.HasOne(c => c.Composition)
+				.WithMany(comp => comp.Comments)
+				.HasForeignKey(c => c.CompositionId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Composition>()
+				.HasOne(c => c.User)
+				.WithMany(u => u.Compositions)
+				.HasForeignKey(c => c.UserId)
+				.OnDelete(DeleteBehavior.Restrict);
+		}
+	}
 }
